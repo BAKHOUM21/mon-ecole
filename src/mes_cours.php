540bb1f373e2f3d273bf1768ee9c8cfa0fb3178b
BAKHOUM21 <bakhoumdemba1@gmail.com>
2024-03-21 14:40:51 +0100

    new  commit

--- src/mes_cours.php
+++ src/mes_cours.php
@@ -14,10 +14,10 @@ $get_cours_by_categories_niveau=get_cours_by_categories_niveau($niveau);
     <div class="card m-auto mt-1 w-100" style="background-color:lightgray;margin-bottom:5%;">
         
         <div class="card-body d-flex">
-            <div class="col-sm-4 bg-image hover-zoom">
+            <!-- <div class="col-sm-4 bg-image hover-zoom">
             <img src="../img/4.jpg" alt="" width="95%">
-            </div>
-        <table id="dt-bordered" class="table table-bordered col-8" cellspacing="0" style="width:65%;float:right;">
+            </div> -->
+        <table id="dt-bordered" class="table table-bordered col-8 m-auto" cellspacing="0" style="width:90%;float:right;">
         
             <thead>
                 <tr>
